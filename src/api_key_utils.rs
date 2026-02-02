f6b6c29a49d13c51954881a8a89a5ca935310f33
Blazej Jaworowski <blazej.jaworowski@gmail.com>
2026-02-02 20:09:28 +0100

    Bump crate version
    Fix api_key_utils

--- src/api_key_utils.rs
+++ src/api_key_utils.rs
@@ -1,42 +1,56 @@
 use std::process::Command;
 
+use parking_lot::Mutex;
 use tracing::debug;
 
 use crate::plugin::ApiKeyGetter;
 
 pub struct ApiKeyCache<T: ApiKeyGetter> {
     getter: T,
-    api_key: Option<String>,
+    api_key: Mutex<Option<String>>,
 }
 
 impl<T: ApiKeyGetter> ApiKeyCache<T> {
     pub fn new(getter: T) -> Self {
         Self {
             getter,
-            api_key: None,
+            api_key: Mutex::new(None),
         }
     }
 }
 
 impl<T: ApiKeyGetter> ApiKeyGetter for ApiKeyCache<T> {
     fn api_key(&self) -> anyhow::Result<String> {
-        if let Some(ref k) = self.api_key {
+        if let Some(ref k) = *self.api_key.lock() {
             return Ok(k.clone());
         }
 
-        self.getter.api_key()
+        let key = self.getter.api_key()?;
+
+        *self.api_key.lock() = Some(key.clone());
+
+        Ok(key)
     }
 }
 
 pub fn pass_getter(store_location: String) -> impl ApiKeyGetter + 'static {
-    let getter = move || {
+    let getter = move || -> anyhow::Result<String> {
         debug!("Getting API key using pass");
 
         let out = Command::new("pass")
             .args(["show", &store_location])
-            .output()?
-            .stdout;
-        let api_key = str::from_utf8(out.as_slice())?.trim();
+            .output()?;
+
+        if !out.status.success() {
+            let error_msg = format!(
+                "pass command failed with status {}: {}",
+                out.status,
+                str::from_utf8(out.stderr.as_slice())?,
+            );
+            return Err(anyhow::Error::msg(error_msg));
+        }
+
+        let api_key = str::from_utf8(out.stdout.as_slice())?.trim();
 
         Ok(api_key.into())
     };
